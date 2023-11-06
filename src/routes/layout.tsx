38857791edb896d7abf62cea7252358c2358fdca
孙腾 <teng_sun@qq.com>
2023-11-06 22:58:54 +0800

    chore: init root page

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,18 @@
+import { Helmet } from '@modern-js/runtime/head';
 import { Outlet } from '@modern-js/runtime/router';
 import 'normalize.css';
 
 export default function Layout() {
   return (
-    <div>
+    <>
+      <Helmet>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
+        />
+      </Helmet>
       <Outlet />
-    </div>
+    </>
   );
 }
