eb5b07382f29d0419e9f968841c068b7b1c4f98f
孙腾 <teng_sun@qq.com>
2023-11-26 01:07:00 +0800

    feat: add file list model

--- modern.config.ts
+++ modern.config.ts
@@ -4,6 +4,7 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 export default defineConfig({
   runtime: {
     router: true,
+    state: true,
   },
   plugins: [appTools()],
 });
--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,5 +1,30 @@
+import { Space, Upload, Button } from '@douyinfe/semi-ui';
+import { useModel } from '@modern-js/runtime/model';
 import Style from './index.module.scss';
+import fileListModel from '@/models/file-list';
 
-const Index = () => <main className={Style.page}>123</main>;
+const Index = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, { setFileList }] = useModel(fileListModel);
+
+  const handleFileChange = (fileList: File[]) => {
+    setFileList(fileList);
+  };
+
+  return (
+    <Space className={Style.page} vertical>
+      <Upload
+        action=""
+        accept="video/*"
+        multiple
+        draggable
+        dragMainText={'点击上传或拖拽上传'}
+        dragSubText="仅支持视频文件"
+        onFileChange={handleFileChange}
+      />
+      <Button>开始重命名</Button>
+    </Space>
+  );
+};
 
 export default Index;
